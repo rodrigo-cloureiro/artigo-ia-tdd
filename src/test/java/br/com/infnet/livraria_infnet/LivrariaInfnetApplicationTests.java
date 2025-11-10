0d8ba3ad7f0cd9c069008c1218f1b0d5282bbdc2
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-10 10:56:59 -0300

    inicializando projeto

---
+++ src/test/java/br/com/infnet/livraria_infnet/LivrariaInfnetApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.infnet.livraria_infnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaInfnetApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
