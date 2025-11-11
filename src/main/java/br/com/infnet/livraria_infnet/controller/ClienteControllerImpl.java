0af8b38430cc1b9377008213881859be3d439abf
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-11 13:22:51 -0300

    implementando busca de cliente por CPF no controller E alterando a anotação @Controller

--- src/main/java/br/com/infnet/livraria_infnet/controller/ClienteControllerImpl.java
+++ src/main/java/br/com/infnet/livraria_infnet/controller/ClienteControllerImpl.java
@@ -3,20 +3,29 @@ package br.com.infnet.livraria_infnet.controller;
 import br.com.infnet.livraria_infnet.model.Cliente;
 import br.com.infnet.livraria_infnet.service.ClienteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
 public class ClienteControllerImpl implements ClienteController {
     private final ClienteService clienteService;
 
-    @GetMapping("/")
+    @Override
+    @GetMapping("")
     public List<Cliente> listar() {
         return clienteService.listar();
+    }
+
+    @Override
+    @GetMapping("/{cpf}")
+    public Optional<Cliente> buscarPorCpf(@PathVariable String cpf) {
+        return clienteService.buscarPorCpf(cpf);
     }
 }
