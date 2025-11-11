3c529fc41540ed338a90afa5b9df3b6537608993
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-11 13:23:33 -0300

    alterando anotação para @RestController

--- src/main/java/br/com/infnet/livraria_infnet/controller/LivroControllerImpl.java
+++ src/main/java/br/com/infnet/livraria_infnet/controller/LivroControllerImpl.java
@@ -3,19 +3,19 @@ package br.com.infnet.livraria_infnet.controller;
 import br.com.infnet.livraria_infnet.model.Livro;
 import br.com.infnet.livraria_infnet.service.LivroService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/livros")
 @RequiredArgsConstructor
 public class LivroControllerImpl implements LivroController {
     private final LivroService livroService;
 
-    @GetMapping("/")
+    @GetMapping("")
     public List<Livro> listar() {
         return livroService.listar();
     }
