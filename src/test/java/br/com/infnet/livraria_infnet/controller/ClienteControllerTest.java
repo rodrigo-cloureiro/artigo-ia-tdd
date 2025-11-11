e4a4ff96a625f7c5819d889ae106ce7a7f1c4cfe
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-10 21:42:09 -0300

    corrigindo o ClienteControllerTest para passar

--- src/test/java/br/com/infnet/livraria_infnet/controller/ClienteControllerTest.java
+++ src/test/java/br/com/infnet/livraria_infnet/controller/ClienteControllerTest.java
@@ -1,12 +1,16 @@
 package br.com.infnet.livraria_infnet.controller;
 
 import br.com.infnet.livraria_infnet.model.Cliente;
+import br.com.infnet.livraria_infnet.model.Endereco;
 import br.com.infnet.livraria_infnet.service.ClienteService;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +25,25 @@ public class ClienteControllerTest {
 
     @BeforeEach
     public void setUp() {
+        cliente = new Cliente(
+                "Lívia Pires",
+                "livia.pires@al.infnet.edu.br",
+                "42084537703",
+                "21999963251",
+                new Endereco(
+                        "Estrada Dom Pedro Augusto",
+                        "539",
+                        "ATP 503",
+                        "Alto da Boa Vista",
+                        "Rio de Janeiro",
+                        "RJ",
+                        "20531250"
+                ),
+                Date.from(LocalDate.of(2000, 5, 5)
+                        .atStartOfDay(ZoneId.of("UTC"))
+                        .toInstant()),
+                true
+        );
         clienteController = new ClienteControllerImpl(clienteService);
     }
 
