66e4b4f23a7a8d1fecf30c0ace5a429648347e4b
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-11 12:39:21 -0300

    criando teste para buscar livro por ISBN

--- src/test/java/br/com/infnet/livraria_infnet/repository/LivroRepositoryTest.java
+++ src/test/java/br/com/infnet/livraria_infnet/repository/LivroRepositoryTest.java
@@ -2,9 +2,9 @@ package br.com.infnet.livraria_infnet.repository;
 
 import br.com.infnet.livraria_infnet.model.Livro;
 import org.junit.jupiter.api.*;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,5 +27,12 @@ public class LivroRepositoryTest {
     public void deveRetornarListaDeLivros() {
         List<Livro> livros = livroRepository.findAll();
         assertEquals(25, livros.size());
+    }
+
+    @Test
+    public void deveRetornarLivroPorIsbn() {
+        Optional<Livro> livroOptional = livroRepository.findByIsbn("9788594318619");
+        assertTrue(livroOptional.isPresent());
+        assertEquals("Memórias Póstumas de Brás Cubas", livroOptional.get().getTitulo());
     }
 }
